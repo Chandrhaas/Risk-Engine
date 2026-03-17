7238908e1513a4470d3b0e5e4ad7b5ab8ffc6272
Harshit Shukla <harshitshukl12@gmail.com>
2026-03-17 21:05:10 +0530

    change the python-c++ bridge from ctypes to pybind11 for simpler and easier code

--- Makefile
+++ Makefile
@@ -1,30 +1,34 @@
 # specifying the compiler
 CXX = clang++
 
-# O3 is optimization to make code faster, wall enables the warnigs, std=c++17 tells c++ version , shared means make a library , 
-# fpic is so that library can be loaded anywhere in the memory
-CXXFLAGS =-O3 -Wall -std=c++17 -shared -fPIC
+INCLUDES = $(shell python -m pybind11 --includes)
 
-#variables
+# FILE EXTENSION: Python expects a specific suffix for C++ extensions on Mac 
+SUFFIX = $(shell python -m pybind11 --extension)
+
+# O3 is optimization, Wall enables warnings, std=c++17 tells c++ version, shared makes a library
+# fPIC allows it to be loaded anywhere in memory.
+# '-undefined dynamic_lookup' is REQUIRED on Mac so clang doesn't panic over missing Python symbols.
+CXXFLAGS = -O3 -Wall -std=c++17 -shared -fPIC -undefined dynamic_lookup
+
+# variables
 srcdir = src_cpp
 buildir = build
-target = $(buildir)/libriskengine.so
+
+# The name MUST match PYBIND11_MODULE(riskengine, m) exactly.
+target = $(buildir)/riskengine$(SUFFIX)
 
 source = $(srcdir)/simulation.cpp
 header = $(srcdir)/simulation.h
 
-
 # this runs when "make" is executed
+all: $(target)
 
-all :$(target)
-
-$(target) : $(source) $(header)
+$(target): $(source) $(header)
 	@mkdir -p $(buildir)
 	@echo "Compiling Risk Engine..."
-	$(CXX) $(CXXFLAGS) $(source) -o $(target)
-	@echo "Compilation Successful"
+	$(CXX) $(CXXFLAGS) $(INCLUDES) $(source) -o $(target)
+	@echo "Compilation Successful!"
 
-clean :
+clean:
 	rm -f $(buildir)/*.so
-	
-
--- src_cpp/simulation.cpp
+++ src_cpp/simulation.cpp
@@ -1,146 +1,127 @@
 #include "simulation.h"
+#include <iostream>
 #include <cmath>
 #include <algorithm>
 #include <random>
 #include <numeric>
-using namespace std;
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h> 
 
-MonteCarloEngine::MonteCarloEngine(int sim , int assets,
-                                    const vector<double>&mean,
-                                    const vector<double>&covs,
-                                    const vector<double> &weights,
-                                    double portfolio_value) :sim(sim), assets(assets),mean(mean),covs(covs),
-                                    weights(weights),portfolio_value(portfolio_value){}
+namespace py = pybind11;
+using namespace std; 
 
-vector<vector<double>> MonteCarloEngine::cholesky_decomp(vector<double>&mat,int n)
-{
-    vector<vector<double>>L(n,vector<double>(n,0.0));
-    //making mat into a lower tirangle matrix
-    for(int i=0;i<n;i++)
+MonteCarloEngine::MonteCarloEngine(int assets, int sim) 
+    : assets(assets), sim(sim) {}
+
+// Vectors handle their own memory. No manual delete needed.
+MonteCarloEngine::~MonteCarloEngine() {}
+
+SimResult MonteCarloEngine::runSimulation(
+    double initial_portfolio_value,
+    const vector<double>& weights,
+    const vector<double>& mean_returns,
+    const vector<double>& cov_matrix) 
     {
-        for(int j=0;j<=i;j++)
-        {
-            double sum=0;
-            for(int k=0;k<j;k++)
-            sum+=L[i][k]*L[j][k];
 
-            if(i==j)
-            L[i][j]=sqrt(mat[i*n+i]-sum);
-            else
-            L[i][j]=(1.0/L[j][j]*(mat[i*n+j]-sum));
+    //Cholesky Decomposition , creating the L Matrix (in a flat vector)
+    vector<double> L(assets * assets, 0.0);
+    for (int i = 0; i < assets; i++) 
+    {
+        for (int j = 0; j <= i; j++) 
+        {
+            double sum = 0.0;
+            if (j == i) 
+            { 
+                for (int k = 0; k < j; k++)
+                    sum +=(L[j * assets + k])*(L[j * assets + k]);
+                L[j * assets + j] = sqrt(cov_matrix[j * assets + j] - sum);
+            } 
+            else 
+            {      
+                for (int k = 0; k < j; k++)
+                    sum += (L[i * assets + k] * L[j * assets + k]);
+                L[i * assets + j] = (cov_matrix[i * assets + j] - sum) / L[j * assets + j];
+            }
         }
     }
-    return L;
-}
-void MonteCarloEngine::run()
-{
+    //  Random Number Generator
     mt19937 generator(random_device{}());
-    normal_distribution<double>distribution(0.0,1.0);
+    normal_distribution<double> distribution(0.0, 1.0);
 
-    auto L = cholesky_decomp(covs,assets);
-
-    vector<double>final_portfolio_values;
+    vector<double> final_portfolio_values;
     // resize fills the array with zeros and reserve keeps it empty saving us some work since we do not need those zeros
     final_portfolio_values.reserve(sim);
 
     vector<double> correlated_randoms(assets, 0.0);
     vector<double> uncorrelated_randoms(assets, 0.0);
-    for(int i=0;i<sim;i++)
+
+    // The Monte Carlo Loop
+    for(int i = 0; i < sim; i++) 
     {
-        //generating random number vector
-        for(int j=0;j<assets;j++)
-        uncorrelated_randoms[j]=distribution(generator);
+        
+        for(int j = 0; j < assets; j++)
+            uncorrelated_randoms[j] = distribution(generator);
 
         fill(correlated_randoms.begin(), correlated_randoms.end(), 0.0);
 
-        // multiplying the random number vector with cholesky decomp matrix to get an array of correlated random numbers
-        for(int k=0;k<assets;k++)
+        // Matrix Multiplication 
+        for(int k = 0; k < assets; k++) 
         {
-            for(int c=0;c<=k;c++)
-           correlated_randoms[k]+=L[k][c]*uncorrelated_randoms[c];
-        }
+            for(int c = 0; c <= k; c++) 
+            {
+                correlated_randoms[k] += L[k * assets + c] * uncorrelated_randoms[c];
+            }
+        } 
         
+        double current_value = 0.0;
 
-        double current_value=0.0;
-
-        //using geometric brownian motion : Price_t = Price_0 * exp( (Mean - 0.5 * Var) + StdDev * Random )
-        for (int a=0;a<assets;a++)
+        // Geometric Brownian Motion : Price_t = Price_0 * exp( (Mean - 0.5 * Var) + StdDev * Random )
+        for (int a = 0; a < assets; a++) 
         {
-            // volatility = std deviation
-            double volatility = sqrt(covs[a * assets + a]);
-
-            // drift = mean - volatility drag 
-            double drift = mean[a] - 0.5 * volatility * volatility;
-
-            //the random shock
+            double volatility = sqrt(cov_matrix[a * assets + a]);
+            double drift = mean_returns[a] - 0.5 * volatility * volatility;
             double diffusion = volatility * correlated_randoms[a];
-
-            double return_factor=exp(drift+diffusion);
-
-            current_value+=(portfolio_value*weights[a])*return_factor;
+            
+            double return_factor = exp(drift + diffusion);
+            current_value += (initial_portfolio_value * weights[a]) * return_factor;
         }
         final_portfolio_values.push_back(current_value);
     }
 
+   
+    sort(final_portfolio_values.begin(), final_portfolio_values.end());
 
-sort(final_portfolio_values.begin(),final_portfolio_values.end());
+    int ind_95 = static_cast<int>(sim * 0.05);
+    int ind_99=static_cast<int>(sim*0.01);
+    
+    SimResult result;
+    result.var95 = initial_portfolio_value - final_portfolio_values[ind_95];
+    result.var99 = initial_portfolio_value - final_portfolio_values[ind_99];
 
-int ind_95= static_cast<int>(sim*0.05);
-int ind_99=static_cast<int>(sim*0.01);
+    //calculating mean loss
+    double loss = 0.0;
+    for (int i = 0; i < ind_95; i++) 
+    {
+        loss += (initial_portfolio_value - final_portfolio_values[i]);
+    }
+    result.mean_loss = (ind_95 > 0) ? (loss / ind_95) : 0.0; 
 
-result.var95 = portfolio_value - final_portfolio_values[ind_95];
-result.var99 = portfolio_value- final_portfolio_values[ind_99];
-
-// calculating mean loss 
-double loss=0.0;
-for (int i=0;i<ind_95;i++)
-{
-    loss+= portfolio_value-final_portfolio_values[i];
-}
-result.meanloss=loss/ind_95;
-
-}
-
-Simresult MonteCarloEngine::get_result()
-{
     return result;
 }
 
-extern "C"{
-    // python sends the data here
-    MonteCarloEngine* MonteCarloEngine_new(int sim, int assets, 
-                                           double* mean, double* cov, 
-                                           double* weights, double portfolio_value)
-    {
-        vector<double>v_means(mean,mean+assets);
-        vector<double> v_cov(cov, cov + (assets * assets));
-        vector<double> v_weights(weights, weights + assets);
+PYBIND11_MODULE(riskengine, m) 
+{
+    // Bind the struct
+    py::class_<SimResult>(m, "SimResult")
+        .def_readwrite("var95", &SimResult::var95)
+        .def_readwrite("var99", &SimResult::var99);
 
-        return new MonteCarloEngine(sim, assets, v_means, v_cov, v_weights, portfolio_value);
-    }
-
-    //  Run the math
-    void MonteCarloEngine_run(MonteCarloEngine* engine) {
-        if (engine != nullptr) {
-            engine->run();
-        }
-    }
-
-    // Get the results struct
-    Simresult MonteCarloEngine_getresult(MonteCarloEngine* engine) {
-        if (engine != nullptr) {
-            return engine->get_result();
-        }
-        return {0.0, 0.0, 0.0};  
-    }
-
-    // Destroy the object
-    void MonteCarloEngine_delete(MonteCarloEngine* engine) {
-        if (engine != nullptr) {
-            delete engine;
-        }
-    }
+    // Bind the Engine class
+    py::class_<MonteCarloEngine>(m, "MonteCarloEngine")
+        .def(py::init<int, int>(), py::arg("assets"), py::arg("sim"))
+        .def("runSimulation", &MonteCarloEngine::runSimulation,
+             py::arg("initial_portfolio_value"),
+             py::arg("weights"),
+             py::arg("mean_returns"),
+             py::arg("cov_matrix"));
 }
-
-
-
